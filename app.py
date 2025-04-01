a21d0d837743e65510160ed2b67bf344c68b6381
Radu <radu.ciobotaru.k@gmail.com>
2025-04-01 19:02:01 +0300

    Prima versiune aplicatie birouri

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+# Datele salvate temporar
+bookings = {
+    "Luni": {},
+    "Marți": {},
+    "Miercuri": {},
+    "Joi": {},
+    "Vineri": {}
+}
+birouri = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        ziua = request.form["ziua"]
+        nume = request.form["nume"]
+        birou = int(request.form["birou"])
+        if birou not in bookings[ziua].values():
+            bookings[ziua][nume] = birou
+    return render_template("index.html", bookings=bookings, birouri=birouri)
+
+if __name__ == "__main__":
+    app.run(debug=True)
