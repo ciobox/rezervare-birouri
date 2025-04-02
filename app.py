c2ce1a88d89977f25ec32569fa249218d1762f75
Radu <radu.ciobotaru.k@gmail.com>
2025-04-02 11:55:27 +0300

    update birouri

--- app.py
+++ app.py
@@ -11,10 +11,10 @@ def get_month_view():
     days_in_month = {}
     for day in range(1, 32):
         try:
-            day_date = datetime.date(today.year, today.month, day)
-            day_name = day_date.strftime("%A")
-            days_in_month[day_name] = {
-                "date": day_date,
+            date = datetime.date(today.year, today.month, day)
+            day_name = date.strftime("%A")
+            days_in_month[day] = {
+                "date": date,
                 "name": day_name,
                 "booked": bookings.get(day_name, {})
             }
@@ -29,21 +29,26 @@ def index():
         ziua = request.form["ziua"]
         nume = request.form["nume"]
         birou = int(request.form["birou"])
-        if ziua not in bookings:
-            bookings[ziua] = {}
-        if birou not in bookings[ziua].values():
-            bookings[ziua][nume] = birou
+
+        if birou not in bookings.get(ziua, {}).values():
+            bookings.setdefault(ziua, {})[nume] = birou
         else:
             error = "Birou deja rezervat!"
 
-    days_in_month_view = get_month_view()
     occupied_desks = []
     for zi in bookings:
-        for nume, birou in bookings[zi].items():
+        for _, birou in bookings[zi].items():
             occupied_desks.append(birou)
-    return render_template("index.html", bookings=bookings, birouri=birouri,
-                           days_in_month=days_in_month_view,
-                           occupied_desks=occupied_desks, error=error)
+
+    days_in_month_view = get_month_view()
+    return render_template(
+        "index.html",
+        bookings=bookings,
+        birouri=birouri,
+        days_in_month=days_in_month_view,
+        occupied_desks=occupied_desks,
+        error=error
+    )
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=10000)
